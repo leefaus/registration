95b14348cd50ef309f0ce90f63056662bb20c00e
Christian Weber <webdog@github.com>
2017-01-02 23:30:13 -0600

    Update example seed data with scheduled date

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-partner = Partner.create(:name => "Forest", :country => "UK", :city => "London")
+partner = Partner.create(:name => "Forest", :country => "UK", :city => "London", :scheduled_date => "2017-12-20")
 puts "PARTNER: #{partner.inspect}"
