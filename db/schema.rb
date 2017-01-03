d158bc20706e121ac78c919add083e187de36f94
Christian Weber <webdog@github.com>
2017-01-02 23:30:50 -0600

    Working schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503182329) do
+ActiveRecord::Schema.define(version: 20170103041620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,8 +41,9 @@ ActiveRecord::Schema.define(version: 20160503182329) do
     t.string   "name"
     t.string   "city"
     t.string   "country"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.date     "scheduled_date", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "registrants", force: :cascade do |t|
